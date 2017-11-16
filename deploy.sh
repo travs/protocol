f69196ba138103c85e3066cbd88a7c82bffbe3f8
Travis Jacobs <traviswjacobs@gmail.com>
2017-11-16 17:16:06 +0100

    Add address-book writing

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 set -ex
 
+# globals
+OUT_DIR="./out"
+ADDRESS_BOOK='address-book.json'
+
 # set variables
 ADDRESS_1='0x0015248b433a62fb2d17e19163449616510926b6'
 ADDRESS_2='0x00248d782b4c27b5c6f42feb3f36918c24b211a5'
@@ -9,30 +13,48 @@ ADDRESS_5='0x00b71117fff2739e83cadba788873adce169563b'
 ADDRESS_6='0x00f18cd3ea9a97828861ac9c965d09b94fce746e'
 
 export ETH_FROM="$ADDRESS_1"
-OUT_DIR="./out"
 
 # deploy libraries
-REWARDS_ADDR=$(dapp create libraries/rewards)
-SIMPLEADAPTER_ADDR=$(dapp create exchange/adapter/simpleAdapter)
+REWARDS=$(dapp create libraries/rewards)
+SIMPLE_ADAPTER=$(dapp create exchange/adapter/simpleAdapter)
 
 # link libraries
 SIMPLEADAPTER_PLACEHOLDER='src/exchange/adapter/simpleAdapter.sol:simpleAdapter'
 REWARDS_PLACEHOLDER='src/libraries/rewards.sol:rewards'
 
-LINKS="$SIMPLEADAPTER_PLACEHOLDER:$SIMPLEADAPTER_ADDR $REWARDS_PLACEHOLDER:$REWARDS_ADDR"
+LINKS="$SIMPLEADAPTER_PLACEHOLDER:$SIMPLE_ADAPTER $REWARDS_PLACEHOLDER:$REWARDS"
 (set -x; solc --link --libraries "$LINKS" "${OUT_DIR?}"/**/*.bin)
 
 # deploy contracts
-ETH_ADDR=$(dapp create PreminedAsset "Ether Token" "ETH-T" 18 1000000000)
-EUR_ADDR=$(dapp create PreminedAsset "Euro Token" "EUR-T" 18 1000000000)
-MLN_ADDR=$(dapp create PreminedAsset "Melon Token" "MLN-T" 18 1000000000)
-DATAFEED_ADDR=$(dapp create datafeeds/DataFeed $MLN_ADDR 0 0)
-SIMPLEMARKET_ADDR=$(dapp create exchange/thirdparty/SimpleMarket)
-SPHERE_ADDR=$(dapp create sphere/Sphere $DATAFEED_ADDR $SIMPLEMARKET_ADDR)
-PARTICIPATION_ADDR=$(dapp create participation/Participation)
-RISKMGMT_ADDR=$(dapp create riskmgmt/RMMakeOrders)
-#GOVERNANCE_ADDR=$(dapp create system/Governance [] 0 100000) # TODO: can't encode [] yet (paritytech/ethabi/issues/65)
-VERSION_ADDR=$(dapp create version/Version 1 $ADDRESS_2 $MLN_ADDR --gas 6000000) # TODO: change the version num and governance address (depends on above)
+ETH_TOKEN=$(dapp create PreminedAsset "Ether Token" "ETH-T" 18 1000000000)
+EUR_TOKEN=$(dapp create PreminedAsset "Euro Token" "EUR-T" 18 1000000000)
+MLN_TOKEN=$(dapp create PreminedAsset "Melon Token" "MLN-T" 18 1000000000)
+DATAFEED=$(dapp create datafeeds/DataFeed $MLN_TOKEN 0 0)
+SIMPLE_MARKET=$(dapp create exchange/thirdparty/SimpleMarket)
+SPHERE=$(dapp create sphere/Sphere $DATAFEED $SIMPLEMARKET)
+PARTICIPATION=$(dapp create participation/Participation)
+RISK_MGMT=$(dapp create riskmgmt/RMMakeOrders)
+GOVERNANCE=$(dapp create system/Governance [4b28c7f4beb488989a2e01333eb67511e07dff31] 0 100000) # TODO: can't encode [] yet (paritytech/ethabi/issues/65), so using a mock address
+VERSION=$(dapp create version/Version 1 $ADDRESS_2 $MLN_TOKEN --gas 6000000) # TODO: change the version num and governance address (depends on above)
 
+# TODO: make this more dynamic (e.g. writing only to one environment object
+cat <<EOF > $ADDRESS_BOOK
+{
+  "development": {
+    "DataFeed": "$DATAFEED",
+    "SimpleMarket": "$SIMPLE_MARKET",
+    "Sphere": "$SPHERE",
+    "Participation": "$PARTICIPATION",
+    "RMMakeOrders": "$RISK_MGMT",
+    "Governance": "$GOVERNANCE",
+    "rewards": "$REWARDS",
+    "simpleAdapter": "$SIMPLE_ADAPTER",
+    "Version": "$VERSION",
+    "MlnToken": "$MLN_TOKEN",
+    "EurToken": "$EUR_TOKEN",
+    "EthToken": "$ETH_TOKEN"
+  }
+}
+EOF
 
-
+cat "$ADDRESS_BOOK"
